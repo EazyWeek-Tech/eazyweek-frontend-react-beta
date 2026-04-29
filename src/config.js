09a38541ec40b80aadfca8560291491f9d841a30
AnkurBhayana <50107595+AnkurBhayana@users.noreply.github.com>
2026-04-29 21:24:04 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // src/config.js
-export const API_BASE_URL = 'https://insightweb-hkhqgch8hadvcbb0.uaenorth-01.azurewebsites.net';
+export const API_BASE_URL = 'https://eazyweek-beta-api-bdega7b0gza8g8bh.uaenorth-01.azurewebsites.net';
